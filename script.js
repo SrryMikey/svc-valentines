51ed4d9a2cc14f05f7296acdd8d64d04599667f1
SrryMikey <70109885+SrryMikey@users.noreply.github.com>
2025-02-07 23:40:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 // Function to handle button click events
 function selectOption(option) {
     // Check which option was clicked
@@ -30,14 +28,14 @@ function flashRainbowColors(callback) {
     var interval = setInterval(function() {
         document.body.style.backgroundColor = colors[i];
         i = (i + 1) % colors.length;
-    }, 200); // Change color every 200 milliseconds
+    }, 150); // Change color every 200 milliseconds
     setTimeout(function() {
         clearInterval(interval);
         document.body.style.backgroundColor = ''; // Reset background color
         if (callback) {
             callback();
         }
-    }, 2000); // Flash colors for 2 seconds
+    }, 3000); // Flash colors for 2 seconds
 }
 
 // Function to display the cat.gif initially
@@ -59,19 +57,28 @@ function displayCat() {
 // Function to display the cat-heart.gif
 function displayCatHeart() {
     // Clear existing content in the image container
+
     document.getElementById('image-container').innerHTML = '';
     // Get the container where the image will be displayed
+
     var imageContainer = document.getElementById('image-container');
     // Create a new Image element for the cat-heart
+
     var catHeartImage = new Image();
     // Set the source (file path) for the cat-heart image
+
+    
+
     catHeartImage.src = 'cat-heart.gif'; // Assuming the cat-heart image is named "cat-heart.gif"
     // Set alternative text for the image (for accessibility)
+    
     catHeartImage.alt = 'Cat Heart';
     // When the cat-heart image is fully loaded, add it to the image container
+
     catHeartImage.onload = function() {
         imageContainer.appendChild(catHeartImage);
         // Hide the options container
+
         document.getElementById('options').style.display = 'none';
     };
 }
